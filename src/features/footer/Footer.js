130d0a5234531e7326745288ef6a4d133f9333d5
Long (Edward) T. NGUYEN <edward@enclave.vn>
2023-04-21 11:28:54 +0700

    master: update - check all completed, remove all completed task, assign color

--- src/features/footer/Footer.js
+++ src/features/footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 const RemainingTodos = ({ count }) => {
   const suffix = count === 1 ? '' : 's'
@@ -13,19 +13,25 @@ const RemainingTodos = ({ count }) => {
 }
 
 const Footer = () => {
+  const dispatch = useDispatch()
+
   const todosRemaining = useSelector((state) => {
     const uncompletedTodos = state.todos.filter((todo) => !todo.completed)
     return uncompletedTodos.length
   })
 
+  const onMarkCompletedClicked = () => dispatch({ type: 'todos/allCompleted' })
+  const onClearCompletedClicked = () =>
+    dispatch({ type: 'todos/completedCleared' })
+    
   return (
     <footer className="footer">
       <div className="actions">
         <h5>Actions</h5>
-        <button className="button" onClick={() => {}}>
+        <button className="button" onClick={onMarkCompletedClicked}>
           Mark All Completed
         </button>
-        <button className="button" onClick={() => {}}>
+        <button className="button" onClick={onClearCompletedClicked}>
           Clear Completed
         </button>
       </div>
--- src/features/todos/TodoListItem.js
+++ src/features/todos/TodoListItem.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { availableColors, capitalize } from '../filters/colors'
 import { ReactComponent as TimesSolid } from './times-solid.svg'
 
@@ -7,16 +7,34 @@ const selectTodoById = (state, todoId) => {
   return state.todos.find((todo) => todo.id === todoId)
 }
 
-const colorOptions = availableColors.map((c) => (
-  <option key={c} value={c}>
-    {capitalize(c)}
-  </option>
-))
-
 const TodoListItem = ({ id }) => {
   const todo = useSelector((state) => selectTodoById(state, id))
   console.log('todo === ', todo)
   const { text, completed, color } = todo
+
+  const dispatch = useDispatch()
+
+  const handleCompletedChanged = () => {
+    dispatch({ type: 'todos/todoToggled', payload: todo.id })
+  }
+
+  const handleColorChanged = (e) => {
+    const color = e.target.value
+    dispatch({
+      type: '',
+      payload: { todoId: todo.id, color },
+    })
+  }
+
+  const onDelete = () => {
+    dispatch({ type: 'todos/todoDeleted', payload: todo.id })
+  }
+
+  const colorOptions = availableColors.map((c) => (
+    <option key={c} value={c}>
+      {capitalize(c)}
+    </option>
+  ))
 
   return (
     <li>
@@ -26,7 +44,7 @@ const TodoListItem = ({ id }) => {
             className="toggle"
             type="checkbox"
             checked={completed}
-            onChange={() => {}}
+            onChange={handleCompletedChanged}
           />
           <div className="todo-text">{text}</div>
         </div>
@@ -35,12 +53,12 @@ const TodoListItem = ({ id }) => {
             className="colorPicker"
             value={color}
             style={{ color }}
-            onChange={() => {}}
+            onChange={handleColorChanged}
           >
             <option value=""></option>
             {colorOptions}
           </select>
-          <button className="destroy" onClick={() => {}}>
+          <button className="destroy" onClick={onDelete}>
             <TimesSolid />
           </button>
         </div>
